6ae53cc9e155a97996575f48b0a41290314fa5d1
Dejan Josifovic <Rajoduo@yahoo.com>
2016-06-22 21:59:52 +0200

    Try out different pin configuration that is probably better for board organization.

--- src/arduino/ArduinoTempReader/ArduinoTempReader.ino
+++ src/arduino/ArduinoTempReader/ArduinoTempReader.ino
@@ -1,20 +1,20 @@
 /*
-  SD card attached to SPI bus as follows:
-  - MOSI - pin 11
-  - MISO - pin 12
-  - CLK - pin 13
-  - CS - pin 4
-
-  Temperature sensor DS18S20 attach as follows:
-  - Red wire - 5V vnc
-  - Blue wire - GRD
-  - Yellow wire - Data - pin 2
-  See: http://bildr.org/2011/07/ds18b20-arduino/
-
-  Real time module attach as follows:
-  - Chip Enable - pin 8
-  - Input/Output - pin 9
-  - Serial Clock - pin 10
+ * 1. SD card module should be attached as follows:
+ * - MOSI - pin 11 ------\
+ * - MISO - pin 12--------> Stanard Arduino SPI interface - must not be changed.
+ * - SCK (CLK) - pin 13--/
+ * - CS - pin 7 - Data, this is configurable (SDCardPin).
+ * 
+ * 2. Temperature sensor (DS18S20) attach as follows:
+ * - Red wire - 5V VCC
+ * - Blue/gray wire - GRD
+ * - Yellow wire - pin 8 - Data, this is configurable (DS18S20_Pin).
+ * See: http://bildr.org/2011/07/ds18b20-arduino/
+ * 
+ * 3. Real time module attach as follows:
+ * - Chip Enable (RST) - pin 2 - (kCePin)
+ * - Input/Output (DAT) - pin 3 - (kIoPin)
+ * - Serial Clock (CLK) - pin 4 - (kSclkPin)
 */
 #include <SPI.h>
 #include <SD.h>
@@ -22,26 +22,34 @@
 #include <stdio.h>
 #include <DS1302.h>
 
-bool debug = false;
+bool debug = true;
 String fileName = "Temp.txt";
-long readDelay = 120000;
+long readDelay = 10000;
 String delimeter = ";";
 
 int cycle = 0;
 
-const int DS18S20_Pin = 2;
-const int SDCardPin = 4;
-const int kCePin   = 8;
-const int kIoPin   = 9;
-const int kSclkPin = 10;
+const int DS18S20_Pin = 8;
+const int SDCardPin = 7;
+const int kCePin = 2;
+const int kIoPin = 3;
+const int kSclkPin = 4;
 
-File myFile;
+File myFile; // Output file.
 OneWire ds(DS18S20_Pin); // Temp reader object.
 DS1302 rtc(kCePin, kIoPin, kSclkPin); // Real time clock reader object.
 
 void setup() {
   debugSerialWrite("Starting initialization...");
   Serial.begin(9600);
+
+  debugSerialWrite("Initializing real time module...");
+  debugSerialWrite("Current time: " + getTime());
+
+  debugSerialWrite("Initializing temperature sensor...");
+  String tmp = "Current temperature: ";
+  tmp += getTemp();
+  debugSerialWrite(tmp);
 
   debugSerialWrite("Initializing SD card...");
   if (!SD.begin(SDCardPin)) {
